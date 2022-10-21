d2ee05359410b8330788110f3db2c1f62d0d27a2
Joan Skënderi <jskenderi@outlook.com>
2022-10-21 20:10:09 +0200

    turtle Commit🔥

---
+++ Square with variables/main.py
@@ -0,0 +1,16 @@
+# Write a program in turtle that uses variables to draw a square.
+
+import turtle
+
+myTurtle = turtle.Turtle()
+
+length = 100
+angle = 90
+
+myTurtle.forward(length)
+myTurtle.left(angle)
+myTurtle.forward(length)
+myTurtle.left(angle)
+myTurtle.forward(length)
+myTurtle.left(angle)
+myTurtle.forward(length)
---
+++ Triangle with variables/main.py
@@ -0,0 +1,14 @@
+# Write a program in turtle that uses variables to draw a triangle.
+
+import turtle
+
+myTurtle = turtle.Turtle()
+
+length = 100
+angle = 120
+
+myTurtle.forward(length)
+myTurtle.left(angle)
+myTurtle.forward(length)
+myTurtle.left(angle)
+myTurtle.forward(length)
