ad10d38a3ecdaf04da0abb452e8a2e89181058f4
Joan Skënderi <jskenderi@outlook.com>
2022-10-22 02:17:34 +0200

    Python Commit🔥

---
+++ Half pyramid of stars/main.py
@@ -0,0 +1,6 @@
+# // Create a half pyramid pattern using * in Python.
+
+for i in range(1, 11):
+    for j in range(i):
+        print("*", end="")
+    print()
---
+++ Number table/main.py
@@ -0,0 +1,6 @@
+# Write a program in Python that displays a table with numbers that multiply with each other.
+
+for i in range(1, 6):
+    for j in range(1, 6):
+        print(i * j, end="\t")
+    print()
