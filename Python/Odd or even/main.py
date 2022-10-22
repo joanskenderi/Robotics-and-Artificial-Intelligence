d440eb59ed2aa9f4be8089bc59398e16c3ccb8a0
Joan Skënderi <jskenderi@outlook.com>
2022-10-22 16:57:13 +0200

    Python Commit🔥

---
+++ Python/Odd or even/main.py
@@ -0,0 +1,11 @@
+# Write a program in Python that checks if a number is odd or even.
+
+def odd_or_even(number):
+    if (number % 2 == 0):
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+
+
+odd_or_even(2)
+odd_or_even(3)
